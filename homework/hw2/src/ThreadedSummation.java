b0a640ed9ed2cd1d8957d178e43f2266ff87a879
Antonio Zea Jr <halosprtn117@gmail.com>
2022-02-08 09:45:58 -0500

    fixes indexing issues and adds histogram
    Uses indexing to properly assign a "fair" number of values to each thread.
    Histogram added for better visualization of data.

--- homework/hw2/src/ThreadedSummation.java
+++ homework/hw2/src/ThreadedSummation.java
@@ -6,28 +6,36 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class ThreadedSummation {
     private static class Summation implements Runnable {
-        static AtomicReference finalSum = new AtomicReference<>(new BigInteger("0"));
+        static AtomicReference<BigInteger> finalSum = new AtomicReference<>(new BigInteger("0"));
         BigInteger interSum;
         List<Integer> randInts;
         int startIndex, endIndex;
 
         private Summation(List<Integer> randInts, int startIndex, int endIndex) {
             interSum = new BigInteger("0");
-            this.randInts=randInts;
-            this.startIndex=startIndex;
-            this.endIndex=endIndex;
+            this.randInts = randInts;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
         }
 
         @Override
         public void run() {
-            for (int i = startIndex; i < endIndex; i++) {
+            for (int i = startIndex; i <= endIndex; i++) {
                 interSum = interSum.add(BigInteger.valueOf(randInts.get(i).longValue()));
             }
-            finalSum.updateAndGet((v)-> ((BigInteger)v).add(interSum));
+            finalSum.updateAndGet(v -> v.add(interSum));
+        }
+
+        public static BigInteger getFinalSum() {
+            return finalSum.get();
+        }
+
+        public static void resetFinalSum() {
+            finalSum = new AtomicReference<>(new BigInteger("0"));
         }
     }
 
-    public static List<Integer> randIntegerArray(int arraySize, int maxValue){
+    public static List<Integer> randIntegerArray(int arraySize, int maxValue) {
         List<Integer> randInts = new ArrayList<>(arraySize);
         Random rand = new Random();
         for (int i = 0; i < arraySize; i++) {
@@ -36,60 +44,70 @@ public class ThreadedSummation {
         return randInts;
     }
 
-    public static long timeTrial(Runnable prog, int numThreads) throws InterruptedException {
+    public static long timeTrial(List<Integer> testData, int numThreads) throws InterruptedException {
         Thread[] threads = new Thread[numThreads];
+
+        int smallStep = testData.size() / numThreads;
+        int overflow = testData.size() % numThreads;
+
+
+        int startIndex, endIndex = -1;
+
         for (int i = 0; i < numThreads; i++) {
-            threads[i] = new Thread(prog);
+
+            startIndex = endIndex + 1;
+            if (i < overflow) {
+                endIndex = startIndex + smallStep;
+            } else {
+                endIndex = startIndex + smallStep - 1;
+            }
+            threads[i] = new Thread(new Summation(testData, startIndex, endIndex));
         }
 
         long start = System.nanoTime();
 
-        for(Thread x: threads){
+        for (Thread x : threads) {
             x.start();
         }
 
-        for(Thread x: threads){
+        for (Thread x : threads) {
             x.join();
         }
 
         long end = System.nanoTime();
+        return end - start;
+    }
 
+    public static void printHistogram(long[] histogram) {
+        long largest = histogram[0];
+        for (long l : histogram) {
+            if (l > largest) {
+                largest = l;
+            }
+        }
 
-        return end-start;
+        for (int i = 0; i < histogram.length; i++) {
+            System.out.println((i + 1) + "\t:" + convertToStars((histogram[i]* 100)/largest));
+        }
+    }
+
+    public static String convertToStars(long nanoSeconds) {
+        StringBuilder stars = new StringBuilder();
+        for (long j = 0; j < nanoSeconds; j++) {
+            stars.append('*');
+        }
+        return stars.toString();
     }
 
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> testData = randIntegerArray(250, 100);
-        Summation temp = new Summation(testData,0, testData.size()/2);
-        Summation temp2 = new Summation(testData,testData.size()/2, testData.size());
+        List<Integer> testData = randIntegerArray(250_000_000, 100);
+        long[] histogram = new long[100];
 
-        Thread t1 = new Thread(temp);
-        Thread t2 = new Thread(temp2);
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
-
-        System.out.println("Sum: "+ temp.finalSum);
-        System.out.println("Sum2: "+ temp2.finalSum);
-
-        Summation temp3 = new Summation(testData,0, testData.size()/2);
-        Summation temp4 = new Summation(testData,testData.size()/2, testData.size());
-
-        Thread t3 = new Thread(temp3);
-        Thread t4 = new Thread(temp4);
-        t3.start();
-        t4.start();
-        t3.join();
-        t4.join();
-
-        System.out.println("Sum: "+ temp3.finalSum);
-        System.out.println("Sum2: "+ temp4.finalSum);
-
-        long numSUm = 0;
-        for (int i = 0; i < testData.size(); i++) {
-            numSUm+= testData.get(i).intValue();
+        for (int i = 0; i < 100; i++) {
+            histogram[i] = timeTrial(testData, i + 1);
+            Summation.resetFinalSum();
         }
-        System.out.println("Manual sum: " + numSUm);
+
+        printHistogram(histogram);
     }
 }
