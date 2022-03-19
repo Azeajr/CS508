9318ddcca56b047a9327621ab9ac165bbc962d96
Antonio Zea Jr <halosprtn117@gmail.com>
2022-03-19 09:53:10 -0400

    fixes the mutex issue not actually making the program thread safe
    The same mutex needs to be shared across all class objects.  Therefore
    making it static made sense.

--- homework/hw2/src/ThreadedSummation.cpp
+++ homework/hw2/src/ThreadedSummation.cpp
@@ -7,32 +7,70 @@
 #include <unistd.h>
 #include <vector>
 
+/**
+ * @brief Summation class meant to facilitate the summation of large array of 
+ * numbers
+ */
 class Summation {
 private:
-  std::mutex mutex;
+  static std::mutex mutex;
 
 public:
+  /**
+   * @brief finalSum  acts as the global sum shared by all instances of the 
+   * class. Each Summation object will have its own intersum.
+   */
   static unsigned long long finalSum;
   unsigned long long interSum;
   std::vector<int> randInts;
   int startIndex, endIndex;
+  /**
+   * @brief Construct a new Summation object
+   * 
+   * @param randInts A vector containing all the integers to be added
+   */
   Summation(std::vector<int> randInts) {
     interSum = 0;
     this->randInts = randInts;
   }
+  /**
+   * @brief Class function that will be run by threads.
+   * 
+   * @param startIndex 
+   * @param endIndex 
+   */
   void run(int startIndex, int endIndex) {
     for (size_t i = startIndex; i <= endIndex; i++) {
       this->interSum += randInts.at(i);
     }
-
+    /**
+     * @brief Putting this thread to sleep to increase the chance that they 
+     * will make the program misbehave
+     */
     std::this_thread::sleep_for(
         std::chrono::milliseconds(1 + std::rand() / ((RAND_MAX + 1u) / 1000)));
-    
+
+    /**
+     * @brief As far as I understand this, lock_guard does the work of locking 
+     * the mutex(mutual exclusive) for a portion of code and unlocking the 
+     * mutex once control leaves the scope in which the lock_guard was created.
+     * 
+     * I expect this to have the same effect as synchronized in java
+     * 
+     * @return std::lock_guard<std::mutex> 
+     */
     std::lock_guard<std::mutex> lock(mutex);
     finalSum += interSum;
   }
 };
 
+/**
+ * @brief Creates a vector of size arraySize with int values from 0 to maxValue inclusive.
+ * 
+ * @param arraySize 
+ * @param maxValue 
+ * @return std::vector<int> 
+ */
 std::vector<int> randIntegerArray(int arraySize, int maxValue) {
   std::vector<int> randInts;
   std::srand(std::time(nullptr));
@@ -42,6 +80,15 @@ std::vector<int> randIntegerArray(int arraySize, int maxValue) {
   return randInts;
 }
 
+/**
+ * @brief Takes the testData and splits up the work of adding the vector's 
+ * integers across numThreads many threads.
+ * 
+ * @param testData Vector of integers to be summed
+ * @param numThreads Number of threads to create
+ * @return std::chrono::microseconds Duration of how long it took to sum the 
+ * integers using this many threads
+ */
 std::chrono::microseconds timeTrial(std::vector<int> testData, int numThreads) {
   Summation::finalSum = 0;
   // Summation *sum = new Summation(testData);
@@ -75,36 +122,27 @@ std::chrono::microseconds timeTrial(std::vector<int> testData, int numThreads) {
 }
 
 unsigned long long Summation::finalSum = 0;
+std::mutex Summation::mutex;
 
 int main(int argc, char const *argv[]) {
 
   std::vector<int> testing = randIntegerArray(10000, INT_MAX - 1);
 
-  // Summation sum1 = Summation(testing);
-  // Summation sum2 = Summation(testing);
+  /**
+   * @brief Test summing the vector using 1 through 100 threads.
+   * 
+   */
+  for (size_t i = 1; i <= 100; i++) {
+    std::cout << "Thread #: " << i
+              << "\tDuration: " << timeTrial(testing, i).count()
+              << " milliseconds" << std::endl;
+    std::cout << "Sum: " << Summation::finalSum << std::endl;
+  }
 
-  // std::cout << "Sum1: " << sum1.finalSum << std::endl;
-  // sum1.run(0, 49);
-  // std::cout << "Sum1: " << sum1.finalSum << std::endl;
-
-  // std::cout << "Sum2: " << sum2.finalSum << std::endl;
-  // sum2.run(50, 99);
-  // std::cout << "Sum2: " << sum2.finalSum << std::endl;
-
-  // std::cout << "Sum1: " << sum1.finalSum << std::endl;
-
-  // for (size_t i = 1; i <= 10; i++) {
-  //   std::cout << "Thread #: " << i
-  //             << "\tDuration: " << timeTrial(testing, i).count()
-  //             << " milliseconds" << std::endl;
-  //   std::cout << "Sum: " << Summation::finalSum << std::endl;
-  // }
-
-  std::cout << "Thread #: " << 100
-            << "\tDuration: " << timeTrial(testing, 100).count()
-            << " milliseconds" << std::endl;
-  std::cout << "Sum: " << Summation::finalSum << std::endl;
-
+  /**
+   * @brief Calculate the sum in the main thread to verify other sums
+   * 
+   */
   unsigned long long temp = 0;
   for (auto &element : testing) {
     temp += element;
